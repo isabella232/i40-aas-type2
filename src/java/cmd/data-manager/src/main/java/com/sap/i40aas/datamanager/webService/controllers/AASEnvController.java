b57cabc83cca0ba8237d8d7f177c533432ad3c3a
D071343 <orthodoxos.kipouridis@sap.com>
2020-08-06 22:47:47 +0200

    added AASEnv controller

--- src/java/cmd/data-manager/src/main/java/com/sap/i40aas/datamanager/webService/controllers/AASEnvController.java
+++ src/java/cmd/data-manager/src/main/java/com/sap/i40aas/datamanager/webService/controllers/AASEnvController.java
@@ -4,9 +4,11 @@ import com.sap.i40aas.datamanager.AASObjectsDeserializer;
 import com.sap.i40aas.datamanager.webService.services.AASEnvObjectsService;
 import identifiables.AssetAdministrationShellEnv;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class AASEnvController {
@@ -18,18 +20,19 @@ public class AASEnvController {
   //need for mock testing
   public AASEnvController(AASEnvObjectsService aasEnvService) {
 
-
     this.aasEnvService = aasEnvService;
   }
 
 
-  @PostMapping("/env/")
-  public void updateSubmodel(@RequestBody String body) {
+  @RequestMapping(value = "/env", method = POST)
+  public String addAASEnv(@RequestBody String body) {
 
 
     //NOTE: we give String in @Requestbody otherwise it will be deserialized with Jackson
     AssetAdministrationShellEnv env = AASObjectsDeserializer.Companion.deserializeAASEnv(body);
     aasEnvService.addAASEnv(env);
+
+    return env.getSubmodels().get(0).getIdShort();
   }
 
 
