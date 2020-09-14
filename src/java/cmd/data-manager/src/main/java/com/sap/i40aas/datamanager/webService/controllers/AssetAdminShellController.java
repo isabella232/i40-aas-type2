f62f4b243af2946ac68a5aae4701c5400a572cee
D071343 <orthodoxos.kipouridis@sap.com>
2020-09-15 00:55:04 +0200

    added AAS persistence

--- src/java/cmd/data-manager/src/main/java/com/sap/i40aas/datamanager/webService/controllers/AssetAdminShellController.java
+++ src/java/cmd/data-manager/src/main/java/com/sap/i40aas/datamanager/webService/controllers/AssetAdminShellController.java
@@ -1,55 +1,100 @@
 package com.sap.i40aas.datamanager.webService.controllers;
 
+import com.sap.i40aas.datamanager.errorHandling.AASObjectValidationException;
+import com.sap.i40aas.datamanager.errorHandling.DuplicateResourceException;
+import com.sap.i40aas.datamanager.validation.IdURLConstraint;
 import com.sap.i40aas.datamanager.webService.services.AssetAdminShellObjectsService;
 import identifiables.AssetAdministrationShell;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import utils.AASObjectsDeserializer;
 
 import java.util.List;
 
 @RestController
+@Validated
 @Slf4j
 
 public class AssetAdminShellController {
 
-
   @Autowired
-  private final AssetAdminShellObjectsService aasService;
+  private final AssetAdminShellObjectsService assetAdminShellObjectsService;
+
+  @IdURLConstraint
+  private String id;
 
   //need for mock testing
-  public AssetAdminShellController(AssetAdminShellObjectsService aasService) {
-    this.aasService = aasService;
+  public AssetAdminShellController(AssetAdminShellObjectsService assetAdminShellObjectsService) {
+    this.assetAdminShellObjectsService = assetAdminShellObjectsService;
   }
 
 
   @GetMapping(value = "/shells")
-  public List<AssetAdministrationShell> getShellsList() {
+  public List<AssetAdministrationShell> getAssetAdministrationShellsList() {
 
     log.info("List AAS request");
-    return aasService.getAllAASs();
+    return assetAdminShellObjectsService.getAllAssetAdministrationShells();
   }
 
   //use the params to filter by HTTP parameters
   @GetMapping(value = "/shells", params = "id")
-  public AssetAdministrationShell getShells(@RequestParam(name = "id", required = false) String id) {
+  public AssetAdministrationShell getAssetAdministrationShells(@RequestParam(name = "id", required = false) @IdURLConstraint String id) {
 
-    return aasService.getAssetAdministrationShell(id);
+    return assetAdminShellObjectsService.getAssetAdministrationShell(id);
   }
 
   @PutMapping("/shells")
-  public void updateAsset(@RequestBody String body, @RequestParam("id") String id) {
+  public ResponseEntity AssetAdministrationShell(@RequestBody String body, @RequestParam("id") @IdURLConstraint String id) throws DuplicateResourceException {
+
+    log.debug("Received PUT /shells with id " + id);
 
     //NOTE: we give String in @Requestbody otherwise it will be deserialized with Jackson. TODO: see if there are alternatives to this
-    AssetAdministrationShell aas = AASObjectsDeserializer.Companion.deserializeAAS(body);
-    aasService.updateAAS(id, aas);
+    AssetAdministrationShell aas;
+    try {
+      aas = AASObjectsDeserializer.Companion.deserializeAAS(body);
+    } catch (Exception ex) {
+      throw new AASObjectValidationException(ex.getMessage());
+    }
+
+    AssetAdministrationShell createdAAS = assetAdminShellObjectsService.createAssetAdministrationShell(id, aas);
+
+    return new ResponseEntity<>(createdAAS, HttpStatus.CREATED);
+  }
+
+  @PatchMapping("/shells")
+  public ResponseEntity updateSubmodel(@RequestBody String body, @RequestParam("id") @IdURLConstraint String id) {
+
+    AssetAdministrationShell aas;
+    try {
+      aas = AASObjectsDeserializer.Companion.deserializeAAS(body);
+    } catch (Exception ex) {
+      throw new AASObjectValidationException(ex.getMessage());
+    }
+    AssetAdministrationShell updatedAAS = assetAdminShellObjectsService.updateAssetAdministrationShell(id, aas);
+
+    return new ResponseEntity<>(updatedAAS, HttpStatus.OK);
+  }
+
+  @PostMapping("/shells")
+  public ResponseEntity updateAssetAdministrationShell(@RequestBody String body) {
+    AssetAdministrationShell aas;
+    try {
+      aas = AASObjectsDeserializer.Companion.deserializeAAS(body);
+    } catch (Exception ex) {
+      throw new AASObjectValidationException(ex.getMessage());
+    }
+    AssetAdministrationShell addedAAS = assetAdminShellObjectsService.addAssetAdministrationShell(aas);
+    return new ResponseEntity<>(addedAAS, HttpStatus.OK);
   }
 
 
   @DeleteMapping("/shells")
-  public void deleteShell(@RequestParam("id") String id) {
+  public void deleteAssetAdministrationShell(@RequestParam("id") @IdURLConstraint String id) {
+    assetAdminShellObjectsService.deleteAssetAdministrationShell(id);
 
-    aasService.deleteAas(id);
   }
 }
--- src/java/cmd/data-manager/src/main/java/com/sap/i40aas/datamanager/webService/services/AssetAdminShellObjectsService.java
+++ src/java/cmd/data-manager/src/main/java/com/sap/i40aas/datamanager/webService/services/AssetAdminShellObjectsService.java
@@ -1,50 +1,84 @@
 package com.sap.i40aas.datamanager.webService.services;
 
+import com.sap.i40aas.datamanager.errorHandling.DuplicateResourceException;
+import com.sap.i40aas.datamanager.persistence.entities.AssetAdministrationShellEntity;
+import com.sap.i40aas.datamanager.persistence.repositories.AssetAdministrationShellRepository;
 import identifiables.AssetAdministrationShell;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import utils.SampleAASFactory;
+import utils.AASObjectsDeserializer;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
 @Service
 public class AssetAdminShellObjectsService {
 
-  private AssetAdminShellObjectsService aasObjectService;
+  private AssetAdminShellObjectsService assetAdminShellObjectsService;
+
+  @Autowired
+  private AssetAdministrationShellRepository aasRepo;
 
 
-  private final List<AssetAdministrationShell> aasArray = new ArrayList<>(Arrays.asList(
-    SampleAASFactory.Companion.getSampleAAS("sampleAas_1"),
-    SampleAASFactory.Companion.getSampleAAS("sampleAas_2")
-  ));
+  public AssetAdministrationShell getAssetAdministrationShell(String id) {
 
-
-  public AssetAdministrationShell getAssetAdministrationShell(String aasId) {
-//TODO: replace idshort with the following
-
-    AssetAdministrationShell aasFound = aasArray.stream().filter(submodel -> submodel.getIdentification().getId().equals(aasId)).findFirst().get();
+    AssetAdministrationShellEntity aasEntityFound = aasRepo.findById(id).get();
+    AssetAdministrationShell aasFound = AASObjectsDeserializer.Companion.deserializeAAS(aasEntityFound.getAasObj());
     return aasFound;
   }
 
 
-  public List<AssetAdministrationShell> getAllAASs() {
-    return aasArray;
+  public List<AssetAdministrationShell> getAllAssetAdministrationShells() {
+    List<AssetAdministrationShell> aasList = new ArrayList<>();
+    aasRepo.findAll().forEach(assetAdministrationShellEntity -> {
+      aasList.add(AASObjectsDeserializer.Companion.deserializeAAS(assetAdministrationShellEntity.getAasObj()));
+    });
+    return aasList;
   }
 
-  public void updateAAS(String aasId, AssetAdministrationShell aas) {
-    if (aasArray.stream().anyMatch(aas1 -> aas.getIdentification().getId().equals(aasId))) {
-      aasArray.set(aasArray.indexOf(aasArray.stream().filter(aas1 -> aas.getIdentification().getId().equals(aasId)).findFirst().get()), aas);
+  public AssetAdministrationShell updateAssetAdministrationShell(String id, AssetAdministrationShell aas) {
+
+    if (aasRepo.findById(id).isPresent()) {
+      AssetAdministrationShellEntity sbE = new AssetAdministrationShellEntity(id, AASObjectsDeserializer.Companion.serializeAAS(aas));
+      aasRepo.save(sbE);
+      return aas;
     } else
-      aasArray.add(aas);
+      throw new java.util.NoSuchElementException();
   }
 
-  public void addAas(AssetAdministrationShell aas) {
-    aasArray.add(aas);
+  public AssetAdministrationShell addAssetAdministrationShell(AssetAdministrationShell aas) {
+
+    AssetAdministrationShellEntity sbE = new AssetAdministrationShellEntity(aas.getIdentification().getId(), AASObjectsDeserializer.Companion.serializeAAS(aas));
+    aasRepo.save(sbE);
+    return aas;
+
   }
 
-  public void deleteAas(String aasId) {
-    aasArray.removeIf(t -> t.getIdentification().getId().equals(aasId));
+  public AssetAdministrationShell createAssetAdministrationShell(String id, AssetAdministrationShell aas) throws DuplicateResourceException {
+// if Id not present create else if already there throw error
+    if (aasRepo.findById(id).isPresent() == false) {
+
+      String aasSer = AASObjectsDeserializer.Companion.serializeAAS(aas);
+
+      AssetAdministrationShellEntity sbE = new AssetAdministrationShellEntity(id, aasSer);
+      aasRepo.save(sbE);
+      return aas;
+    } else
+      throw new DuplicateResourceException();
+
   }
+
+  public AssetAdministrationShell deleteAssetAdministrationShell(String id) {
+    if (aasRepo.findById(id).isPresent()) {
+      //find the Submodel so that it gets returned
+      AssetAdministrationShellEntity aasEntityFound = aasRepo.findById(id).get();
+      AssetAdministrationShell aasFound = AASObjectsDeserializer.Companion.deserializeAAS(aasEntityFound.getAasObj());
+
+      aasRepo.deleteById(id);
+      return aasFound;
+    } else
+      throw new java.util.NoSuchElementException();
+  }
+
 }
