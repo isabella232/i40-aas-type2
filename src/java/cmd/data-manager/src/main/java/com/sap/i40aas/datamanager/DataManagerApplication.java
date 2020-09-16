1f2bfedede309aad958c8a9c215eea2081ef309d
D071343 <orthodoxos.kipouridis@sap.com>
2020-09-16 16:31:23 +0200

    fix loading file

--- src/java/cmd/data-manager/src/main/java/com/sap/i40aas/datamanager/DataManagerApplication.java
+++ src/java/cmd/data-manager/src/main/java/com/sap/i40aas/datamanager/DataManagerApplication.java
@@ -1,14 +1,9 @@
 package com.sap.i40aas.datamanager;
 
-import identifiables.AssetAdministrationShellEnv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
-import utils.AASObjectsDeserializer;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 
 @SpringBootApplication
@@ -17,18 +12,7 @@ public class DataManagerApplication {
   public static void main(String[] args) throws IOException {
     SpringApplication.run(DataManagerApplication.class, args);
 
-    //read an AASEnv.json
-    File resource = new ClassPathResource(
-      "/testAASEnv.json").getFile();
-    String sampleJson = new String(
-      Files.readAllBytes(resource.toPath()));
-
-    //create a deserializer obj
-
-    //parse the json
-    AssetAdministrationShellEnv parsedAASEnv = AASObjectsDeserializer.Companion.deserializeAASEnv(sampleJson);
-
-    System.out.println("Test property idshort: " + parsedAASEnv.getSubmodels().get(0).getIdShort());
+    System.out.println("Data-manager Service started ");
 
 
   }
